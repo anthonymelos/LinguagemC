9dd6318c0ee5d8e5ff93c85316b3cc583a043ff7
tonynho12 <141790503+tonynho12@users.noreply.github.com>
2023-08-25 13:06:32 +0000

    mensagem

---
+++ Exercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char nome[20], telefone[12];
+    int idade;
+    printf("Digite um nome: ");
+    scanf("%s", nome);
+    printf("Informe sua idade: ");
+    scanf("%d", &idade);
+    printf("Informe um número de telefone: ");
+    scanf("%s", telefone);
+    printf("Nome: %s, idade: %d, telefone: %s", nome, idade, telefone);
+    
+}
