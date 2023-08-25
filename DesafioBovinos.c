137e65cc8b07f83b2cba3822c3a2ae60e5e771b8
tonynho12 <141790503+tonynho12@users.noreply.github.com>
2023-08-25 13:37:50 +0000

    ms

--- DesafioBovinos.c
+++ DesafioBovinos.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    printf("");
+    int bovinosEntrada = 10, bovinosNascidos, bovinosMortos, contador, bovinosSaida;
+    printf("Digite uma população de bovinos: ");
+    scanf("%d", &bovinosEntrada);
+
+    for(contador = 1; contador <= 365; contador++){
+        bovinosNascidos = bovinosEntrada /3;
+        bovinosMortos = bovinosEntrada / 4;
+
+
+    }
+    printf("Quantidade de bovinos: %d", bovinosSaida);
+
+    
+   
+
 }
